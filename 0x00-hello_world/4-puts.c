829fb46482bdc7e48aa2484a382edd629ac9b0b2
DTroy22 <trevordarylgmail.com>
2022-10-14 09:36:02 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return: Always 0
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	retun(0);
+	retun (0);
 }
