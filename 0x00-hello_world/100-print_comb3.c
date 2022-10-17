3657404843e7c57d6183b1756d7fa721ed7504e3
DTroy22 <trevordarylgmail.com>
2022-10-17 01:46:55 -0700

    task 10

--- 0x00-hello_world/100-print_comb3.c
+++ 0x00-hello_world/100-print_comb3.c
@@ -19,4 +19,18 @@ int main(void)
 			if (e != i && e < i)
 			{
 				putchar(e);
-				putchsr(i);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
+}
